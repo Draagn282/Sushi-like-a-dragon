15ab7ed02e0842c5297af1b845f0ed98c0cce667
Draagn282 <daanpx@hotmail.com>
2022-02-21 15:56:33 +0100

    update 21-2-2022

--- index.php
+++ index.php
@@ -4,27 +4,31 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./css/styles.css">
     <title>Document</title>
 </head>
 <body>
     <!-- <?php phpinfo(); ?> -->
     <div class="nav_main_div">
         <div class="nav_left_div">
-            <img scr>
         </div>
         <div class="nav_right_div">
-            <a href=""><button>...</button></a>
-            <a href=""><button>...</button></a>
-            <a href=""><button>...</button></a>
+            <a href=""><button>hoofpagina</button></a>
+            <a href=""><button>menu</button></a>
+            <a href=""><button>contact</button></a>
+            <a href=""><button>pagina veranderen</button></a>
         </div>
     </div>
-    <div class="">
+    <div class="base_main_div">
 
     </div>
-    <div>
+    <div class="base_menu_div">
 
     </div>
-    <div>
+    <div class="base_contact_div">
+
+    </div>
+    <div class="foot_main_div">
 
     </div>
 
