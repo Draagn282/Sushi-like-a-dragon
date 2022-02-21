2907f33987c1777ccd93f8ff37a49b4a4cbfb328
Draagn282 <daanpx@hotmail.com>
2022-02-21 15:15:20 +0100

    g

---
+++ index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <!-- <?php phpinfo(); ?> -->
+    <div class="nav_main_div">
+        <div class="nav_left_div">
+            <img scr>
+        </div>
+        <div class="nav_right_div">
+            <a href=""><button>...</button></a>
+            <a href=""><button>...</button></a>
+            <a href=""><button>...</button></a>
+        </div>
+    </div>
+    <div class="">
+
+    </div>
+    <div>
+
+    </div>
+    <div>
+
+    </div>
+
+</body>
+</html>
