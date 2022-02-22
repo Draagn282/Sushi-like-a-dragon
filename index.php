fac509ffc0b49f88776d80d5a5e1d285bdc5e85f
Draagn282 <daanpx@hotmail.com>
2022-02-22 13:53:04 +0100

    update 22-2-2022

--- index.php
+++ index.php
@@ -1,36 +1,150 @@
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="./css/styles.css">
+  <head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link rel="stylesheet" href="./css/styles.css" />
     <title>ryu ga gotoku SUSHI</title>
-</head>
-<body>
+  </head>
+  <body>
     <!-- <?php phpinfo(); ?> -->
     <div class="nav_main_div">
-        <div class="nav_left_div">
-        </div>
-        <div class="nav_right_div">
-            <a href=""><button>hoofpagina</button></a>
-            <a href=""><button>menu</button></a>
-            <a href=""><button>contact</button></a>
-            <a href=""><button>pagina veranderen</button></a>
-        </div>
+      <div class="nav_left_div"></div>
+      <div class="nav_right_div">
+        <a href=""><button>hoofpagina</button></a>
+        <a href=""><button>menu</button></a>
+        <a href=""><button>contact</button></a>
+        <a href=""><button>pagina veranderen</button></a>
+      </div>
     </div>
     <div class="base_main_div">
-
+      <div class="main_left_div">
+        <div class="main_left_upper_div">
+          <div class="upper_txt">
+            <p>Welcome to LIKE A DRAGON SHUSHI</p>
+          </div>
+        </div>
+        <div class="main_left_downer_div">
+          <div class="downer_txt">
+            <p>
+              Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+              Reiciendis, aliquid omnis? Accusantium cum quisquam et qui eaque
+              doloribus excepturi quod. Sequi doloribus similique pariatur.
+              Earum nostrum expedita modi quasi voluptatum, adipisci ducimus
+              amet eos, cumque ex doloremque rerum iusto. Adipisci!
+            </p>
+          </div>
+        </div>
+      </div>
+      <div class="main_right_div"></div>
     </div>
     <div class="base_menu_div">
-
+      <div class="menu_top_div">
+        <p>Menu</p>
+      </div>
+      <div class="menu_div">
+        <div class="menu_card_div">
+          <div class="card_title">
+            <p>single sushi</p>
+          </div>
+          <div class="card_items">
+            <div class="menu_item">
+              <div class="item_picture">
+                <img style="height: 100%" src="./img/Nishiki_fish.png" />
+              </div>
+              <div class="item_description">
+                <p>item description baby</p>
+              </div>
+            </div>
+            <div class="menu_item">
+              <div class="item_picture">
+                <img style="width: 100%" src="./img/Nishiki_fish.png" />
+              </div>
+              <div class="item_description">
+                <p>item description baby</p>
+              </div>
+            </div>
+            <div class="menu_item">
+              <div class="item_picture">
+                <img style="width: 100%" src="./img/Nishiki_fish.png" />
+              </div>
+              <div class="item_description">
+                <p>item description baby</p>
+              </div>
+            </div>
+            <div class="menu_item">
+              <div class="item_picture">
+                <img style="width: 100%" src="./img/Nishiki_fish.png" />
+              </div>
+              <div class="item_description">
+                <p>item description baby</p>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="menu_card_div">
+          <div class="card_title">
+            <p>single sushi</p>
+          </div>
+          <div class="card_items">
+            <div class="menu_item">
+              <div class="item_picture">
+                <img style="width: 100%" src="./img/Nishiki_fish.png" />
+              </div>
+              <div class="item_description">
+                <p>item description baby</p>
+              </div>
+            </div>
+            <div class="menu_item">
+              <div class="item_picture">
+                <img style="width: 100%" src="./img/Nishiki_fish.png" />
+              </div>
+              <div class="item_description">
+                <p>item description baby</p>
+              </div>
+            </div>
+            <div class="menu_item">
+              <div class="item_picture">
+                <img style="width: 100%" src="./img/Nishiki_fish.png" />
+              </div>
+              <div class="item_description">
+                <p>item description baby</p>
+              </div>
+            </div>
+            <div class="menu_item">
+              <div class="item_picture">
+                <img style="width: 100%" src="./img/Nishiki_fish.png" />
+              </div>
+              <div class="item_description">
+                <p>item description baby</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
-    <div class="base_contact_div">
-
-    </div>
+    <div class="base_contact_div"></div>
     <div class="foot_main_div">
-        
+      <div class="footer_middle_div">
+        <div class="footer_logo_div">
+          <img style="height: 100%" src="./img/Nishiki_fish.png" />
+        </div>
+        <ul class="footer_list">
+          <li>...</li>
+          <hr class="footer_hr" />
+          <li>...</li>
+          <li>...</li>
+          <li>...</li>
+        </ul>
+        <ul class="footer_list">
+          <li>...</li>
+          <hr class="footer_hr" />
+          <li>...</li>
+          <li>...</li>
+          <li>...</li>
+        </ul>
+      </div>
     </div>
-
-</body>
+  </body>
 </html>
