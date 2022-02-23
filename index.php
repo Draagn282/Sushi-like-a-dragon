1df455609df2a56bfc0c4b58a60d986d048c8bd5
Draagn282 <daanpx@hotmail.com>
2022-02-23 09:53:03 +0100

    update 23-2-2022

--- index.php
+++ index.php
@@ -5,6 +5,12 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <link rel="stylesheet" href="./css/styles.css" />
+    <link rel="preconnect" href="https://fonts.googleapis.com" />
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+    <link
+      href="https://fonts.googleapis.com/css2?family=Bebas+Neue&family=Finger+Paint&display=swap"
+      rel="stylesheet"
+    />
     <title>ryu ga gotoku SUSHI</title>
   </head>
   <body>
@@ -18,21 +24,22 @@
         <a href=""><button>pagina veranderen</button></a>
       </div>
     </div>
+    <div class="nav_space"></div>
     <div class="base_main_div">
       <div class="main_left_div">
         <div class="main_left_upper_div">
           <div class="upper_txt">
-            <p>Welcome to LIKE A DRAGON SHUSHI</p>
+            <p class="up_top">Welcome to</p>
+            <p class="up_down">LIKE A DRAGON SUSHI</p>
           </div>
         </div>
         <div class="main_left_downer_div">
           <div class="downer_txt">
-            <p>
-              Lorem, ipsum dolor sit amet consectetur adipisicing elit.
-              Reiciendis, aliquid omnis? Accusantium cum quisquam et qui eaque
-              doloribus excepturi quod. Sequi doloribus similique pariatur.
-              Earum nostrum expedita modi quasi voluptatum, adipisci ducimus
-              amet eos, cumque ex doloremque rerum iusto. Adipisci!
+            <p class="downer_p">
+              Hier maken wij sushi. Duh ik bedoel waarom ga je dan naar deze
+              site. om te kijken hoe mooi hij is... HAHAH ik ben geen art
+              designer. Ik zie wel wat mooit is maar om dat zelf te maken ik wel
+              veel gevraagt
             </p>
           </div>
         </div>
@@ -51,10 +58,19 @@
           <div class="card_items">
             <div class="menu_item">
               <div class="item_picture">
-                <img style="height: 100%" src="./img/Nishiki_fish.png" />
+                <img src="./img/Shusi.jpg" />
               </div>
               <div class="item_description">
-                <p>item description baby</p>
+                <p>
+                  ok even wat typen om ervoor te zorgen dat er een beschrijving
+                  staat die ik goed kan stylen zodat ik een mooit site maak ik
+                  ga gewoon door typen todatik ik denk dat ik niet meer hoef
+                </p>
+              </div>
+              <div class="item_cost">
+                <ul>
+                  <li>$: 3,45</li>
+                </ul>
               </div>
             </div>
             <div class="menu_item">
