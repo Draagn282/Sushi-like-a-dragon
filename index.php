d0bcbd9de973f4bdbc0e8dbbbe32daf25efc68fc
Draagn282 <daanpx@hotmail.com>
2022-03-11 13:22:47 +0100

    update ? idk ik ga dit toch nooit kijken

--- index.php
+++ index.php
@@ -7,10 +7,7 @@
     <link rel="stylesheet" href="./css/styles.css" />
     <link rel="preconnect" href="https://fonts.googleapis.com" />
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
-    <link
-      href="https://fonts.googleapis.com/css2?family=Bebas+Neue&family=Finger+Paint&display=swap"
-      rel="stylesheet"
-    />
+    <link href="https://fonts.googleapis.com/css2?family=Bebas+Neue&family=Finger+Paint&display=swap" rel="stylesheet"/>
     <title>ryu ga gotoku SUSHI</title>
   </head>
   <body>
@@ -152,6 +149,11 @@
               <div class="item_picture">
                 <img src="./img/Shusi.jpg" />
               </div>
+              <div class="item_title">
+                <p>
+                  Sushi met vis
+                </p>
+              </div>
               <div class="item_description">
                 <p>
                   ok even wat typen om ervoor te zorgen dat er een beschrijving
@@ -167,6 +169,11 @@
               <div class="item_picture">
                 <img src="./img/Shusi.jpg" />
               </div>
+              <div class="item_title">
+                <p>
+                  Sushi met vis
+                </p>
+              </div>
               <div class="item_description">
                 <p>
                   ok even wat typen om ervoor te zorgen dat er een beschrijving
@@ -182,11 +189,25 @@
               <div class="item_picture">
                 <img src="./img/Shusi.jpg" />
               </div>
+              <div class="item_title">
+                <p>
+                  Sushi met vis
+                </p>
+              </div>
               <div class="item_description">
                 <p>
-                  ok even wat typen om ervoor te zorgen dat er een beschrijving
+                  <form action="backroom.php" method="post"> 
+                    <label for="fname">First name:</label><br>
+                    <input type="text" id="fname" name="fname"><br>
+                    <label for="fname">Last name:</label><br>
+                    <input type="text" id="Lname" name="Lname"><br>
+                    <label for="fname"> Age:</label><br>
+                    <input type="text" id="age" name="age"><br>
+                    <input type="submit" name="sub" value="Submit ur soul">
+                  </form>  
+                <!-- ok even wat typen om ervoor te zorgen dat er een beschrijving
                   staat die ik goed kan stylen zodat ik een mooit site maak ik
-                  ga gewoon door typen todatik ik denk dat ik niet meer hoef
+                  ga gewoon door typen todatik ik denk dat ik niet meer hoef -->
                 </p>
               </div>
               <div class="item_cost">
@@ -196,6 +217,11 @@
             <div class="menu_item">
               <div class="item_picture">
                 <img src="./img/Shusi.jpg" />
+              </div>
+              <div class="item_title">
+                <p>
+                  Sushi met vis
+                </p>
               </div>
               <div class="item_description">
                 <p>
@@ -215,23 +241,94 @@
     </div>
     <div class="nav_space">
     </div>
+    </div>
     <div class="base_contact_div">
       <div class="contact_title_div">
         <p>
           Contact
         </p>
       </div>
-      <div class="contact_left">
-        <div class="contact_form">
-          <div class="contact_email"></div>
-          <div class="contact_message"></div>
-          <div class="contact_revieuw"></div>
+      <div class="contact_div">
+        <div class="contact_left">
+          <div class="contact_form">
+            <div class="contact_email">
+                  <p class="contact_t">
+                    Email
+                  </p>
+                  <div class="email_ownmail">
+                    <label for="email">Enter your email:</label>
+                  <input type="email" id="email" name="email">
+                  <input type="submit">
+                  </div>
+                  
+                
+              <div class="email_ourmail">
+                <p>
+                if you want to mail me directly. 
+              </p>
+              <p><a href="1186391@student.roc-nijmegen.nl">click here</a></p>
+              </div>
+              
+            </div>
+            <div class="contact_message">
+              <p class="contact_t">
+                Message?
+              </p>
+              <p>
+                want to send us a message?
+              </p>
+              <textarea id="w3review" name="w3review" rows="4" cols="50" placeholder="stop hier je grootste depressie momenten. Praat er over"></textarea>
+            </div>
+            <div class="contact_revieuw">
+              <p class="contact_t">
+                Revieuw
+              </p>
+              <p>
+                revieuw like IGN plz
+              </p>
+              <textarea id="w3review" name="w3review" rows="6" cols="50" placeholder="look at the fokkin cod revieuws and give us that number"></textarea>
+              <div class="revieuw_star">
+               <input type="radio" id="html" name="fav_language" value="HTML">
+              <label for="html">1</label><br>
+              <input type="radio" id="html" name="fav_language" value="HTML">
+              <label for="html">2</label><br>
+              <input type="radio" id="html" name="fav_language" value="HTML">
+              <label for="html">3</label><br>
+              <input type="radio" id="html" name="fav_language" value="HTML">
+              <label for="html">4</label><br>
+              <input type="radio" id="html" name="fav_language" value="HTML">
+              <label for="html">5</label><br> 
+              </div>
+            </div>
+          </div>
         </div>
-      </div>
-      <div class="contact_right">
-        <div class="contact_addnew">
-          <div class="contact_recipe"></div>
-          <div class="contact_ideas"></div>
+        <div class="contact_right">
+          <div class="contact_addnew">
+            <div class="contact_recipe">
+              <p>
+                want to add new recipie?
+              </p>
+              <p>
+                what is in it?
+              </p>
+              <textarea id="w3review" name="w3review" rows="4" cols="50" placeholder="dont say stamin x"></textarea>
+              <p>
+                how much money?
+              </p>
+              <textarea id="w3review" name="w3review" rows="4" cols="50" placeholder="dont say stamin x"></textarea>
+             </div> 
+             <div class="contact_ideas">
+                <p>
+                  any ideas you want to share
+                </p>
+                <textarea id="w3review" name="w3review" rows="4" cols="50" placeholder="dont say stamin x"></textarea> 
+              </div>
+          <div class="contact_comments">
+            <p>
+              comment on aour site
+            </p>
+            <textarea id="w3review" name="w3review" rows="4" cols="50" placeholder="dont say stamin x"></textarea> 
+          </div>
         </div>
       </div>
     </div>
@@ -256,6 +353,10 @@
         </ul>
       </div>
     </div>
+    <?php
+      $protagonistname = "Kiryu";
+      echo "battle for the dreams baby!" .  $protagonistname . " is the best protagonist i have played";
+    ?>
     <script src="./js/main.js"></script>
   </body>
 </html>
